5e25b7b91221d5cdffd63e5f2679cddcdc909551
brave <40793988@163.com>
2016-11-24 15:36:40 +0800

    修复英雄之旅标志

--- src/app/gameData/CarnivalData.lua
+++ src/app/gameData/CarnivalData.lua
@@ -54,8 +54,12 @@ NetWork:addNetWorkListener({ 2, 33 }, onAllBZ)
 
 --英雄之旅标志
 local onLvCheng = function(event)
-    CarnivalData.lvChengBZ = true
-    CarnivalData:updateBz()
+    for i, v in pairs(event.tb) do
+        if CarnivalData.lvCheng[v].day <= PlayerData.eventAttr.day then
+            CarnivalData.lvChengBZ = true
+            CarnivalData:updateBz()
+        end
+    end
 end
 NetWork:addNetWorkListener({ 33, 3 }, onLvCheng)
 
